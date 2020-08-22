58b013add22faf6ac31e1078a85ce8e3e0e360e9
Sarah Myriam <sarahmyriam@Sarahs-MacBook-Pro.local>
2020-08-22 01:29:11 -0400

    new changes to javaScript

--- 02-Homework copy/Develop/script.js
+++ 02-Homework copy/Develop/script.js
@@ -2,50 +2,61 @@
 var generateBtn = document.querySelector("#generate");
 
 
-//generate random password
+//*generate random password
 function passwordPrompts(){
 
-  //Arrays: Uppercase, lowercase, numeric, special characters
+  //!Arrays: Uppercase, lowercase, numeric, special characters
 var upperCaseArray= "ABCDEFGHIJKLMNOPQRSTUVWYZ".split("");
 var lowerCaseArray= "abcdefghijklmnopqrstuvwyz".split ("");
 var numberArray= "0123456789".split("");
 var specialCharsArray= "@%+\\/'!#$^?:,(){}[]~-_.]".split("");
+var chars =[];
 
-//check if prompt is true/false
-var confrimUpperCase = false;
-var confrimLowerCase = false;
-var confrimNumericCase = false;
-var confrimSpecialCharacters = false;
-var length="";
-var password ="";
+//?create for loop to choose password chars
+for(var i=0; i<= Array.length; i++){
+  password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length-1)));
 }
+
+//todo:check if prompt is true/false
+var confirmUpperCase;
+var confirmLowerCase;
+var confirmNumericCase;
+var confirmSpecialChars;
+var chars;
+
+}
+
  //generate password
 function generatePassword(){
   length = document.getElementById("number").value;
-  confrimUpperCase = document.getElementById("uppercase").checked;
-  console.log (confrimUpperCase)
-  confrimlowerCase = document.getElementById("lowercase").checked;
-  console.log (confrimlowerCase)
-  confrimNumericCase = document.getElementById("numericcase").checked;
-  console.log (confrimNumericCase)
-  confrimSpecialChars = document.getElementById("specialchars").checked;
-  console.log (confrimspecialrChars)
 
-  //funtion confirm
-  if (confrimUpperCase == "false"){
+  confirmUpperCase = document.getElementById("uppercase").checked;
+  console.log (confirmUpperCase)
+
+  confirmLowerCase = document.getElementById("lowercase").checked;
+  console.log (confirmLowerCase)
+
+  confirmNumericCase = document.getElementById("numeric").checked;
+  console.log (confirmNumericCase)
+
+  confirmSpecialChars = document.getElementById("specialchars").checked;
+  console.log (confirmSpecialChars)
+
+  //!funtion confirm
+  if (confirmUpperCase == "false"){
     alert ("must choose one Uppercase");
     }
-  if (confrimlowerCase == "false"){
+  if (confirmLowerCase == "false"){
     alert ("must choose one lowercase");
     }
-  if (confrimNumericCase == "false"){
+  if (confirmNumericCase == "false"){
     alert ("must choose one number");
     }
-  if (confrimspecialrChars == "false"){
+  if (confirmSpecialChars == "false"){
     alert ("must choose one specialchars");
     }
 
-  // if(length is less than 8 or length is greater than 128)
+  //*if(length is less than 8 or length is greater than 128)
   if (length < 8 ||  length > 128 ) {
     alert("error, password must be greather than 8 and less than 128")
   };
@@ -53,21 +64,30 @@ function generatePassword(){
 }
 
 
-// Write password to the #password input
+//todo: Write password to the #password input
 function writePassword() {
-  console.log();
-  var password = generatePassword();
+  console.log("execute password");
+  var password = generatePassword("number");
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+//?:Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 console.log(generateBtn)
 
-    //set password length
+    //*set password length
     let length = document.getElementById("password").value;
 
+//PSEUDOCODE
+1-//?click button to generatePassword
+2-//todo:
+3-//*
+4-//!
+5-//
+6-//?
+7-//*
+8-//todo:
 
