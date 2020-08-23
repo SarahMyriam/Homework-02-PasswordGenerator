5a7a9905f23e1120f8a2fbeda25cb8595cb8168b
Sarah Myriam <sarahmyriam@Sarahs-MacBook-Pro.local>
2020-08-22 21:23:50 -0400

    added pseudo code

--- 02-Homework copy/Develop/script.js
+++ 02-Homework copy/Develop/script.js
@@ -1,93 +1,108 @@
-// Assignment Code
+//assignment code
 var generateBtn = document.querySelector("#generate");
-
 
 //*generate random password
 function passwordPrompts(){
 
-  //!Arrays: Uppercase, lowercase, numeric, special characters
-var upperCaseArray= "ABCDEFGHIJKLMNOPQRSTUVWYZ".split("");
-var lowerCaseArray= "abcdefghijklmnopqrstuvwyz".split ("");
-var numberArray= "0123456789".split("");
-var specialCharsArray= "@%+\\/'!#$^?:,(){}[]~-_.]".split("");
-var chars =[];
+//!Arrays: Uppercase, lowercase, numeric, special characters
+    var upperCaseArray= "ABCDEFGHIJKLMNOPQRSTUVWYZ".split("");
+    var lowerCaseArray= "abcdefghijklmnopqrstuvwyz".split ("");
+    var numberArray= "0123456789".split("");
+    var specialCharsArray= "@%+\\/'!#$^?:,(){}[]~-_.]".split("");
+    var chars =[];
+
+//check if prompt is true/false
+    var confrimUpperCase = false;
+    var confrimLowerCase = false;
+    var confrimNumericCase = false;
+    var confrimSpecialCharacters = false;
+    var length="";
+    var password ="";
 
 //?create for loop to choose password chars
-for(var i=0; i<= Array.length; i++){
-  password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length-1)));
-}
+    for(var i=0; i<= Array.length; i++){
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length-1)));
+    }
 
 //todo:check if prompt is true/false
-var confirmUpperCase;
-var confirmLowerCase;
-var confirmNumericCase;
-var confirmSpecialChars;
-var chars;
+    var confirmUpperCase;
+    var confirmLowerCase;
+    var confirmNumericCase;
+    var confirmSpecialChars;
+    var chars;
 
 }
 
- //generate password
-function generatePassword(){
-  length = document.getElementById("number").value;
+//generate password
+    function generatePassword(){
+    if (confrimUpperCase == "false"){
+    length = document.getElementById("number").value;
 
-  confirmUpperCase = document.getElementById("uppercase").checked;
-  console.log (confirmUpperCase)
+    confrimUpperCase = document.getElementById("uppercase").checked;
+    console.log (confrimUpperCase)
 
-  confirmLowerCase = document.getElementById("lowercase").checked;
-  console.log (confirmLowerCase)
+    confrimlowerCase = document.getElementById("lowercase").checked;
+    console.log (confrimlowerCase)
 
-  confirmNumericCase = document.getElementById("numeric").checked;
-  console.log (confirmNumericCase)
+    confrimNumericCase = document.getElementById("numericcase").checked;
+    console.log (confrimNumericCase)
 
-  confirmSpecialChars = document.getElementById("specialchars").checked;
-  console.log (confirmSpecialChars)
-
-  //!funtion confirm
-  if (confirmUpperCase == "false"){
-    alert ("must choose one Uppercase");
-    }
-  if (confirmLowerCase == "false"){
-    alert ("must choose one lowercase");
-    }
-  if (confirmNumericCase == "false"){
-    alert ("must choose one number");
-    }
-  if (confirmSpecialChars == "false"){
-    alert ("must choose one specialchars");
+    confrimSpecialChars = document.getElementById("specialchars").checked;
+    console.log (confrimspecialrChars)
     }
 
-  //*if(length is less than 8 or length is greater than 128)
-  if (length < 8 ||  length > 128 ) {
-    alert("error, password must be greather than 8 and less than 128")
-  };
-  
+//!funtion confirm
+    if (confirmUpperCase == "false"){
+        alert ("must choose one Uppercase");
+        }
+    if (confrimlowerCase == "false"){
+    if (confirmLowerCase == "false"){
+        alert ("must choose one lowercase");
+        }
+    if (confrimNumericCase == "false"){
+    if (confirmNumericCase == "false"){
+        alert ("must choose one number");
+        }
+    if (confrimspecialrChars == "false"){
+    if (confirmSpecialChars == "false"){
+        alert ("must choose one specialchars");
+        }
+
+// if(length is less than 8 or length is greater than 128)
+    if (length < 8 ||  length > 128 ) {
+        alert("remember, password must be greather than 8 and less than 128")
+    };
+
 }
 
 
+// Write password to the #password input
 //todo: Write password to the #password input
-function writePassword() {
-  console.log("execute password");
-  var password = generatePassword("number");
-  var passwordText = document.querySelector("#password");
+    function writePassword() {
+    console.log("execute password");
+    var password = generatePassword("number");
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
+
 
 //?:Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-console.log(generateBtn)
+    generateBtn.addEventListener("click", writePassword);
+    console.log(generateBtn)
 
-    //*set password length
+    
+//*set password length
     let length = document.getElementById("password").value;
 
-//PSEUDOCODE
-1-//?click button to generatePassword
-2-//todo:
-3-//*
-4-//!
-5-//
-6-//?
-7-//*
-8-//todo:
-
+//!PSEUDOCODE
+    /**
+     *1-click button to generatePassword
+    *2-Get password options from users 
+    *3-prompt user to choose length between 8 and 128
+    *4-Get all possible characters from the options and the characters
+    *5-Add these possible characters to result that we are going to return
+    *6-Swap some of the letters in the result array with the characters to ensure that the password has all options the user wants
+    *7-Return the string version of result array with result. 
+    */ 
